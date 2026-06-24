fe6cb2c2a3be765ffdaef5a4d83460d657b5b6fe
DesaleRohit <rohitdesale2005@gmail.com>
2026-06-24 23:35:20 +0530

    [MODIFIED]:Rohit - insertion operation perform

--- SpringJDBCmodule/src/main/java/com/rohit/main/App.java
+++ SpringJDBCmodule/src/main/java/com/rohit/main/App.java
@@ -1,13 +1,35 @@
 package com.rohit.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.rohit.resources.JavaConfig;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		String name = "Gagan";
+		String course = "SpringBoot";
+		String city = "Pune";
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+		
+		JdbcTemplate jdbcTemplate = context.getBean(JdbcTemplate.class);
+
+		String query = "insert into user values(?,?,?)";
+
+		int count = jdbcTemplate.update(query, name, course, city);
+
+		if (count > 0) {
+			System.out.println("Data inserted");
+		} else {
+			System.out.println("Process failed");
+		}
+
+	}
 }
--- SpringJDBCmodule/src/main/java/com/rohit/resources/JavaConfig.java
+++ SpringJDBCmodule/src/main/java/com/rohit/resources/JavaConfig.java
@@ -1,5 +1,30 @@
 package com.rohit.resources;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
 public class JavaConfig {
 
+	@Bean
+	public DriverManagerDataSource getDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/springjdbc");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		return dataSource;
+	}
+
+	@Bean
+	public JdbcTemplate getTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate();
+
+		jdbcTemplate.setDataSource(getDataSource());
+
+		return jdbcTemplate;
+	}
 }
